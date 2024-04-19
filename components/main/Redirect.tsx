601f28d988b92cc5ee42ed97aa48ae0b0820d6d9
haari006 <128443332+haari006@users.noreply.github.com>
2024-04-19 17:59:28 +0800

    Remove loader animation from Redirect component

--- components/main/Redirect.tsx
+++ components/main/Redirect.tsx
@@ -13,7 +13,6 @@ export default function Redirect() {
   // While redirecting, you can show a loader or any other placeholder content
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="rounded-md h-12 w-12 border-4 border-t-4 border-blue-500 animate-spin"></div>
     </div>
   );
 }
